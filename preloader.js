12c563bcf009046d5f61536ed7c252a9a2f52485
uniquebiwas <98706637+uniquebiwas@users.noreply.github.com>
2023-06-24 20:25:49 +0545

    Add files via upload
    all changes added

---
+++ preloader.js
@@ -0,0 +1,8 @@
+function showPreloader() {
+    document.getElementById("cont").style.display = "block";
+  }
+  
+  function hidePreloader() {
+    document.getElementById("cont").style.display = "none";
+  }
+  //https://dribbble.com/shots/2099057-loading
