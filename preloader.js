efe3db43c2681f0774bf89eead4c2ea1bce124b1
Unique Biwas <uniquebiwas@gmail.com>
2023-06-26 18:01:24 +0545

    changes done

--- preloader.js
+++ preloader.js
@@ -1,8 +1,7 @@
 function showPreloader() {
-    document.getElementById("cont").style.display = "block";
-  }
-  
-  function hidePreloader() {
-    document.getElementById("cont").style.display = "none";
-  }
-  //https://dribbble.com/shots/2099057-loading
+  document.getElementById("preloader").style.display = "block";
+}
+
+function hidePreloader() {
+  document.getElementById("preloader").style.display = "none";
+}
