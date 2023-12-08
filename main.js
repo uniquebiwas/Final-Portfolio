4b67559013c88e9a0894a4701f7df81ffd4ebe67
uniquebiwas <uniquebiwas@gmail.com>
2023-12-08 19:57:19 +0545

    game added

--- main.js
+++ main.js
@@ -41,49 +41,44 @@ button.addEventListener('click', function () {
          
         })
         //  console.log(data.main.temp)
-        .catch(err => alert("Wrong City Name..!!"))
+        .catch(err => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Wrong City Name"
+          });
+        });
         // wrong city name error showing part 
         // console.log(CityName.value)
 }
 )
 //api request for weather to display
 fetch('https://api.openweathermap.org/data/2.5/weather?q=kathmandu&appid=c8f7b805243cd8de19a2b0397192fd57')
-.then(response => response.json())
-        
-.then(data => 
+  .then(response => response.json())
+  .then(data => {
+    // ... (your existing code for handling data)
 
-   { 
-       //here the console check code
-       console.log(data.main.temp)
-       console.log(data.main.pressure)
-       console.log(data.main.humidity)
-       console.log(data.name)
-       console.log(data.weather[0].main)
-       console.log(data.wind.speed)
-       console.log(data.wind.deg)
+    const celcius = Math.round(data.main.temp - 273);
 
-       const celcius = Math.round(data.main.temp-273);
-       
+    // variable for calling to display the result
+    var display = `
+      <h2>City Name = ${data.name}, ${data.sys.country}</h2>
+      <h4>Weather Condition = ${data.weather[0].main}</h4>
+      <p>Temperature = ${celcius}°C</p>
+      <p>Pressure = ${data.main.pressure} hpa</p>
+      <p>Humidity = ${data.main.humidity}%</p>
+      <p>Wind Speed = ${data.wind.speed} m/s</p>
+      <p>Direction = ${data.wind.deg}°</p>
+    `;
 
-//variable for calling to display the result
-var display= `
+    // main code for display
+    document.getElementById("display").innerHTML = display;
+  })
+  .catch(err => {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Wrong City Name"
+    });
+  });
 
-
-<h2 > City Name = ${data.name},${data.sys.country} </h2>
-<h4 > Weather Condition = ${data.weather[0].main}</h4>
-<p > Temperature = ${celcius}°C</p>
-<p> Pressure = ${data.main.pressure} hpa</p>
-<p> Humidity = ${data.main.humidity}%</p>
-
-<p > Wind Speed  = ${data.wind.speed} m/s </p>
-<p> Direction = ${data.wind.deg}°</p>
-
-`
-  // main code for display
-document.getElementById("display").innerHTML = display;
-
-
-})
-//  console.log(data.main.temp)
-.catch(err => alert("Wrong City Name..!!"))
-// console.log(CityName.value)
