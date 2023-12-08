bac4bc71c44db015acfb21cda0d8c39dc80ebb93
uniquebiwas <uniquebiwas@gmail.com>
2023-12-08 20:29:01 +0545

    game added

--- preloader.js
+++ preloader.js
@@ -5,3 +5,13 @@ function showPreloader() {
 function hidePreloader() {
   document.getElementById("preloader").style.display = "none";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const extraMenu = document.querySelector('.extra-menu');
+  const extraSubmenu = document.querySelector('.extra-submenu');
+
+  extraMenu.addEventListener('click', function () {
+    // Toggle the visibility of the submenu
+    extraSubmenu.style.display = extraSubmenu.style.display === 'block' ? 'none' : 'block';
+  });
+});
